e5c7072ecea9526be23af2ae6b9ac958ecd70c62
Emrys <emryspotter1234@gmail.com>
2017-11-12 20:00:38 +0630

    before stabbing  with dagger

--- app/src/main/java/io/github/myolwin00/sunshine/SunshineApp.kt
+++ app/src/main/java/io/github/myolwin00/sunshine/SunshineApp.kt
@@ -3,21 +3,22 @@ package io.github.myolwin00.sunshine
 import android.app.Application
 import io.github.myolwin00.sunshine.di.AppComponent
 import io.github.myolwin00.sunshine.di.AppModule
+import io.github.myolwin00.sunshine.di.DaggerAppComponent
 
 /**
  * Created by myolwin00 on 11/11/17.
  */
 class SunshineApp : Application() {
 
-    /*private val appComponent: AppComponent by lazy {
+    private val appComponent: AppComponent by lazy {
         DaggerAppComponent.builder().appModule(AppModule(this)).build()
-    }*/
+    }
 
     override fun onCreate() {
         super.onCreate()
 
-        //appComponent.inject(this)
+        appComponent.inject(this)
     }
 
-    // appComponent(): AppComponent = appComponent
+    fun appComponent(): AppComponent = appComponent
 }
