719c36acec01998805769a0d7c20f63dfb12958b
emrys <emryspotter1234@gmail.com>
2018-02-20 00:19:19 +0630

    update readme, add timber to debug only

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 - Architecture components (LiveData, ViewModel, Room)
 - Dependency Injection (Dagger 2)
 - Reactive Programming (RxJava 2)
+- Custom Logs (Timber)
 
 ## Screenshots
 ![screenshot_2017-11-13-20-41-59-760_io github myolwin00 sunshine](https://user-images.githubusercontent.com/19741333/32729992-073a3fdc-c8b4-11e7-8730-2fbff659c2f4.png)
--- app/src/main/java/io/github/myolwin00/sunshine/SunshineApp.kt
+++ app/src/main/java/io/github/myolwin00/sunshine/SunshineApp.kt
@@ -1,9 +1,11 @@
 package io.github.myolwin00.sunshine
 
 import android.app.Application
+import android.os.Debug
 import io.github.myolwin00.sunshine.di.AppComponent
 import io.github.myolwin00.sunshine.di.AppModule
 import io.github.myolwin00.sunshine.di.DaggerAppComponent
+import timber.log.Timber
 
 /**
  * Created by myolwin00 on 11/11/17.
@@ -18,6 +20,10 @@ class SunshineApp : Application() {
         super.onCreate()
 
         appComponent.inject(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
     fun appComponent(): AppComponent = appComponent
