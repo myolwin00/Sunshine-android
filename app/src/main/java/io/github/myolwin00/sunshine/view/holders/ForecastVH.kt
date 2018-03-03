6080877ad2ebc42b55f548ce95d723b0a2f49976
emrys <emryspotter1234@gmail.com>
2018-03-03 20:33:06 +0630

    remove kotlin extension findviewbyid on onBindViewHolder

--- app/src/main/java/io/github/myolwin00/sunshine/view/holders/ForecastVH.kt
+++ app/src/main/java/io/github/myolwin00/sunshine/view/holders/ForecastVH.kt
@@ -2,6 +2,7 @@ package io.github.myolwin00.sunshine.view.holders
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.TextView
 import io.github.myolwin00.sunshine.data.Forecast
 import io.github.myolwin00.sunshine.utils.SunshineUtils.*
 import kotlinx.android.synthetic.main.item_forecast.view.*
@@ -11,16 +12,21 @@ import kotlinx.android.synthetic.main.item_forecast.view.*
  */
 class ForecastVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+    private val tvDate = itemView.tv_date
+    private val tvHighTemp = itemView.tv_high_temperature
+    private val tvLowTemp = itemView.tv_low_temperature
+    private val tvWeatherDesc = itemView.tv_weather_desc
+
     private lateinit var mForecast: Forecast
 
     fun bind(forecast: Forecast) {
         mForecast = forecast
 
-        itemView.tv_date.text = DateFormatUtil.formatDate(mForecast.mDt!!)
-        itemView.tv_high_temperature.text = TemperatureUtil.formatTemperature(
-                itemView.context, forecast.mMain?.mTempMax)
-        itemView.tv_low_temperature.text = TemperatureUtil.formatTemperature(
-                itemView.context, forecast.mMain?.mTempMin)
-        itemView.tv_weather_desc.text = WeatherUtil.getWeatherDescStr(mForecast.mWeather)
+        tvDate.text = DateFormatUtil.formatDate(mForecast.mDt!!)
+        tvHighTemp.text = TemperatureUtil.formatTemperature(itemView.context,
+                forecast.mMain?.mTempMax)
+        tvLowTemp.text = TemperatureUtil.formatTemperature(itemView.context,
+                forecast.mMain?.mTempMin)
+        tvWeatherDesc.text = WeatherUtil.getWeatherDescStr(mForecast.mWeather)
     }
 }
